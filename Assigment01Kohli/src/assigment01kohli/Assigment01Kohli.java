1a39111c241d386b7dccf47f647896acdb07276c
kohlr10 <kohlr10@DESKTOP-EKKHL0Q.myfiosgateway.com>
2021-02-15 22:57:00 -0500

    Printing Even Number from 0 to 100

---
+++ Assigment01Kohli/src/assigment01kohli/Assigment01Kohli.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assigment01kohli;
+
+/**
+ *
+ * @author kohlr10
+ */
+public class Assigment01Kohli {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Printing out the Statement on screen
+         System.out.println("Even Number from 0 to 100:");
+        // Using the for loop to get number from 0 to 100
+        for (int i = 0; i <= 100; i++){
+           
+            // This statement will only pick even numbers
+            if(i%2 == 0){
+            // Printing I with even numbers    
+            System.out.println(i);
+            
+            }
+        }
+    }
+    
+}
